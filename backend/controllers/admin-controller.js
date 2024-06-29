948a207d9257e84432741e2258a6bced69709225
devamdoshi212 <devamdoshi212@gmail.com>
2024-06-29 11:24:36 +0530

    edit add HR

--- backend/controllers/admin-controller.js
+++ backend/controllers/admin-controller.js
@@ -3,11 +3,12 @@ const { CustomError } = require("../utils/router-utils");
 const userModel = require("../models/user-models");
 const { ok200 } = require("../utils/response-utils");
 const { sendMailToPersons } = require("../utils/mail-utils");
+const { mongoose } = require("mongoose");
 function generatePassword(length) {
   return crypto.randomBytes(length).toString("base64").slice(0, length);
 }
 async function addEmployeeAndHr(req, res, next) {
-  const { role, fullname, email } = req.body;
+  const { role, fullname, email, grievanceTypeId = null } = req.body;
   if (!role || !fullname || !email) {
     throw new CustomError("Invalid Request", 400);
   }
@@ -22,6 +23,7 @@ async function addEmployeeAndHr(req, res, next) {
     fullname,
     email,
     password,
+    grievanceTypeId: new mongoose.Types.ObjectId(grievanceTypeId),
   });
 
   await newUser.save();
